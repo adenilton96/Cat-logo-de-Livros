0ae4b8fe4cd2ac35941e9bb1b0a08ddc069a273c
adenilton96 <adenlton96@live.com>
2024-11-04 00:01:28 -0300

    trocando icone do navegador

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,8 @@
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     
-    <link rel="icon" href="{{ asset('path/to/your/favicon.ico') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('storage/img/livros.png') }}" type="image/x-icon">
+    
 
     <style>
         /* Ocultar a div #app inicialmente */
